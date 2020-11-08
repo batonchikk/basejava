d374b91ee50e03cb81a76cc701e8f6e1d8071d24
batonchikk <kolian0071@yandex.ru>
2020-11-08 20:24:05 +0300

    add access modifier

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -61,7 +61,7 @@ public class MainArray {
         }
     }
 
-    static void printAll() {
+    private static void printAll() {
         Resume[] all = ARRAY_STORAGE.getAll();
         System.out.println("----------------------------");
         if (all.length == 0) {
