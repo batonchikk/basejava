0eb1072bb367a6565c8ff8cdf40e92e6dce1cb29
batonchikk <kolian0071@yandex.ru>
2020-11-08 11:13:11 +0300

    add pakages

---
+++ src/com/urise/webapp/model/Resume.java
@@ -0,0 +1,23 @@
+package com.urise.webapp.model;
+
+/**
+ * Initial resume class
+ */
+public class Resume {
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    // Unique identifier
+    String uuid;
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}
---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,60 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        storage[size] = r;
+        size++;
+    }
+
+    public Resume get(String uuid) {
+        Resume resume = null;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i])) {
+                resume = storage[i];
+            }
+        }
+        return resume;
+    }
+
+    public void delete(String uuid) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                index = i;
+            }
+        }
+        if (index >= 0) {
+            for (int i = index; i < size - 1; i++) {
+                storage[i] = storage[i + 1];
+            }
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+}
